5d5528eb8dea261f8c849cfeeffb969aabb169f7
ValSche <82173740+ValSche@users.noreply.github.com>
2021-12-04 21:05:06 +0100

    Angebotsseite überarbeitet
    Zusätzlich nun die Möglichkeit die Uhrzeit anzugeben sowie Form-Validierung (passende Inhalte)

--- project_knock_knock/werkzeuge/CreateWerkzeug.py
+++ project_knock_knock/werkzeuge/CreateWerkzeug.py
@@ -1,32 +1,34 @@
 from flask import Flask, render_template, Blueprint, request, redirect
-from Models import Offers
+from Models import comOffers
 from extensions import db
-
+from datetime import datetime
 
 create = Blueprint('create', __name__)
 
 
 @create.route('/create', methods=['POST', 'GET'])
-def createOffer():
+def createComOffer():
     if request.method == 'POST':
         content_start = request.form['start']
         content_ende = request.form['ende']
+        content_zeit = request.form['zeit']
         content_geld = request.form['geld']
-        new_offer = Offers(start = content_start, end = content_ende, money = content_geld)
+        zeitAlsPythonObjekt = datetime.strptime(content_zeit, '%Y-%m-%dT%H:%M')
+        new_ComOffer = comOffers(start = content_start, end = content_ende, time = zeitAlsPythonObjekt, money = content_geld)
         try:
-            db.session.add(new_offer)
+            db.session.add(new_ComOffer)
             db.session.commit()
             return redirect('/create')
         except:
-            return 'An Error occured, while trying to add your offer :('
-
+            #return 'An Error occured, while trying to add your offer :('
+            return content_zeit
     else:
-        allOffers = Offers.query.order_by(Offers.id).all()
+        allOffers = comOffers.query.order_by(comOffers.id).all()
         return render_template('create.html', view_name ='Create', allOffers=allOffers)
 
 @create.route('/delete/<int:id>')
 def delete(id):
-    offer_to_delete = Offers.query.get_or_404(id)
+    offer_to_delete = comOffers.query.get_or_404(id)
     try:
         db.session.delete(offer_to_delete)
         db.session.commit()
