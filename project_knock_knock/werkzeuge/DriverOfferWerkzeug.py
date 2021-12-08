7197aa664f3f425db7a428d8362835fc46783799
joe-hannes <johannes.idelberger@googlemail.com>
2021-12-08 23:41:00 +0100

    removed uneccesary code

--- project_knock_knock/werkzeuge/DriverOfferWerkzeug.py
+++ project_knock_knock/werkzeuge/DriverOfferWerkzeug.py
@@ -1,51 +1,34 @@
-from flask import Flask, render_template, Blueprint, request, redirect, url_for
-from flask_user import current_user, login_required
+from flask import Flask, render_template, Blueprint, request, redirect
+from flask_user import current_user
 from Models import DriverOffers, WorkingTime
 from extensions import db
 from datetime import datetime, timedelta
-from forms.driverOffer_form import DriverOfferForm
-import re
 
 driverOffer = Blueprint('driverOffer', __name__)
 
 
 @driverOffer.route('/driverOffer', methods=['POST', 'GET'])
-@login_required
 def createDriverOffer():
-
-    form = DriverOfferForm()
-
-
-
-
-
-    if form.validate_on_submit():
+    if request.method == 'POST':
         content_ort = request.form['ort']
         content_fahrzeug = request.form['fahrzeug']
-        content_verfügbarVon = request.form['verfügbarVon']
-        content_verfügbarBis = request.form['verfügbarBis']
-        content_zeitMo = request.form['from_zeitMo']
-        content_zeitDi = request.form['from_zeitDi']
-        content_zeitMi = request.form['from_zeitMi']
-        content_zeitDo = request.form['from_zeitDo']
-        content_zeitFr = request.form['from_zeitFr']
-        content_zeitSa = request.form['from_zeitSa']
-        content_zeitSo = request.form['from_zeitSo']
+        content_von = request.form['start_date']
+        content_bis = request.form['end_date']
         content_preis = request.form['preis']
-        content_umkreis = request.form['umkreis']
-        content_bemerkungen = request.form['bemerkungen']
+        content_radius = request.form['radius']
+        content_text = request.form['bemerkungen']
+        vonAlsPythonObjekt = datetime.strptime(content_von, '%Y-%m-%dT%H:%M')
+        bisAlsPythonObjekt = datetime.strptime(content_bis, '%Y-%m-%dT%H:%M')
 
 
-        # add work time information from form to database and append it to current user
         i = 0
         for results in request.form:
-            if (results == "from_zeitMo" or results == "from_zeitDi" or results == "from_zeitMi" or \
-            results == "from_zeitDo" or results == "from_zeitFr" or results == "from_zeitSa" or \
-            results == "from_zeitSo"):
+            if (results == "monday" or results == "tuesday" or results == "wednesday" or \
+                results == "thursday" or results == "friday" or results == "saturday" or \
+                results == "sunday"):
 
                 bis = datetime.strptime(request.form[results],'%H:%M')
                 bis += timedelta(hours=8)
-                bis.strftime("%H:%M")
                 working_time = WorkingTime(
                     weekday = i,
                     start_time = request.form[results],
@@ -55,32 +38,24 @@ def createDriverOffer():
                 working_time.driver.append(current_user)
                 i+=1
 
-        formatted_datetime = content_start_zeit[:8] + '-' + content_start_zeit[9:]
-
-        verfügbarVonAlsPythonObjekt = datetime.strptime(content_verfügbarVon, '%d.%m.%Y-%H:%M')
-        verfügbarBisAlsPythonObjekt = datetime.strptime(content_verfügbarBis, '%d.%m.%Y-%H:%M')
-
         driver_offer = DriverOffers(
             location = content_ort,
             vehicle = content_fahrzeug,
-            start_time = verfügbarVonAlsPythonObjekt,
-            end_time = verfügbarVonAlsPythonObjekt,
+            start_time = vonAlsPythonObjekt,
+            end_time = bisAlsPythonObjekt,
             kilometerpreis = content_preis,
-            radius = content_umkreis,
-            text = content_bemerkungen,
+            radius = content_radius,
+            text = content_text
         )
-
         try:
             db.session.add(driver_offer)
-            # associate a driver_offer with a specific user
-            driver_offer.creator.append(current_user)
             db.session.commit()
             return redirect('/driverOffer')
         except:
             return 'An Error occured, while trying to add your offer :('
     else:
         allDriverOffers = DriverOffers.query.order_by(DriverOffers.id).all()
-        return render_template('driverOffer.html', view_name ='Driver Offer', allDriverOffers=allDriverOffers, form=form)
+        return render_template('driverOffer.html', view_name ='Driver Offer', allDriverOffers=allDriverOffers)
 
 @driverOffer.route('/deleteDriverOffer/<int:id>')
 def delete(id):
