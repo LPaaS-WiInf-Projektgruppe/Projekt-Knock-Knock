614e5b271b2c1256aeab53bee57e3fea76f4eb51
ValSche <82173740+ValSche@users.noreply.github.com>
2021-12-10 12:39:20 +0100

    ErrorMessage FlaskWTF angepasst

--- project_knock_knock/forms/comOffer_form.py
+++ project_knock_knock/forms/comOffer_form.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, InputRequired, NumberRange, Regexp
 
 GELD_ERROR = "Please enter a a valid amount of money! (Must be a number und have a maximum amount of 2 decimal places: e.g 12.99 Euro) "
 LOCATION_ERROR = "Please enter a valid location! (Must be an actual place in the format: street + number)"
-TIME_ERROR = "Please enter a valid time! (Must be in the format DD.MM.YYYY-HH:MM and can't be in the past)"
+TIME_ERROR = "Please enter a valid time! (Must be in the format DD.MM.YYYY HH:MM and can't be in the past)"
 DURATION_ERROR = "Must be a valid duration! (Must be a number and have a maximum amount of 1 decimal place: e.g. 1.5. Furthermore duration has to be be larger than 0 and smaller than 7 in order to be accepted.) "
 
 #"^[A-z]*$"
