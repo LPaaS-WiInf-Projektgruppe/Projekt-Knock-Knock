c4cb543f9ecbd95d653587c684ba6e7746e6d2d6
ValSche <82173740+ValSche@users.noreply.github.com>
2021-12-17 23:16:30 +0100

    Test Parameterübergabe

--- project_knock_knock/werkzeuge/ChatWerkzeug.py
+++ project_knock_knock/werkzeuge/ChatWerkzeug.py
@@ -1,14 +1,21 @@
-from flask_user import login_required
-from flask import Flask, render_template, Blueprint
+from flask_user import login_required, current_user
+from flask import Flask, render_template, Blueprint, request, redirect
+from Models import ExchangedMessages, User
+from extensions import db
 
 chat = Blueprint('chat', __name__)
 
 
 @chat.route('/chat')
-# @login_required
-def chat_func():
+@login_required
+def chat_test():
 
-    return render_template(
-        "chat.html",
-        view_name='Chat',
-        )
+    return render_template("chat.html", view_name='Chat',)
+
+
+
+@chat.route('/chat/<int:dude_id>')
+@login_required
+def chat_func(dude_id):
+    result = User.query.filter_by(id = dude_id).first()
+    return render_template("chat.html", view_name='Chat',dude = result)
