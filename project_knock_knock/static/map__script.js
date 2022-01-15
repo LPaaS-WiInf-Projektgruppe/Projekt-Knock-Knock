544e5f36b9870ed9362439980912a90ca0c1a1c8
joe-hannes <johannes.idelberger@googlemail.com>
2022-01-15 11:47:29 +0100

    zwischenspeicher

---
+++ project_knock_knock/static/map__script.js
@@ -0,0 +1,63 @@
+$("document").ready( () =>
+{
+
+
+  var map = L.map('map', {preferCanvas: true}).setView([53.599813099076165, 9.932808452732685], 13);
+  // var marker = L.marker([53.599813099076165, 9.932808452732685]).addTo(map);
+
+
+  // https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/dark-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoiam9leTE1MTciLCJhIjoiY2t5MXJwODFyMDIzczJubjExbm90aHc3YSJ9.8Ildv_V7RY38fLBWWxmt2Q'
+    }).addTo(map);
+
+
+
+  async function get_coordinates(name)
+  {
+    response = await fetch("/com_offer_coordinates", {method: "POST"})
+    coordinates = await response.json()
+
+  return await coordinates;
+}
+
+(async () => {
+
+  coordinates = await get_coordinates()
+
+  for (i = 0; i < coordinates.length;  i++)
+  {
+  // var marker = L.marker(coordinates[i][1]).addTo(map);
+
+  if i == 0 or i == 1 or i == 2 or i == 3 or i == 4 or i == 24:
+    var polygon = L.polygon([
+        coordinates[i][1],
+        coordinates[i][2]
+    ]).addTo(map);
+
+  if (i != 24)
+  {
+    var marker =  L.circle(coordinates[i][1], 100, {color:"green"}).addTo(map);
+    var marker =  L.circle(coordinates[i][2], 100, {color:"red"}).addTo(map);
+    var polygon = L.polygon([
+        coordinates[i][1],
+        coordinates[i][2]
+    ]).addTo(map);
+  }
+  else
+  {
+    var marker =  L.circle(coordinates[i][1], 200, {color:"yellow"}).addTo(map);
+  }
+}
+
+
+
+})()
+
+
+})
--- project_knock_knock/static/profil__script.js
+++ project_knock_knock/static/profil__script.js
@@ -1,5 +1,102 @@
 
 
+$("document").ready( () =>
+{
+
+  // response = await fetch("/com_offer_coordinates", {method: "POST"})
+  // coordinates = await response.json()
+
+  //
+  // for (coordinate in coodinates)
+  // {
+  //   var marker = L.marker([coordinates[2][0], coordinates[2][1]]).addTo(map);
+  //   var marker = L.marker([coordinates[2][0], coordinates[2][1]]).addTo(map);
+  // }
+
+  var map = L.map('map', {preferCanvas: true}).setView([53.599813099076165, 9.932808452732685], 13);
+  // var marker = L.marker([53.599813099076165, 9.932808452732685]).addTo(map);
+
+
+  // https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/dark-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoiam9leTE1MTciLCJhIjoiY2t5MXJwODFyMDIzczJubjExbm90aHc3YSJ9.8Ildv_V7RY38fLBWWxmt2Q'
+    }).addTo(map);
+
+  // var polygon = L.polygon([
+  //     [53.599813099076165, 9.932808452732685],
+  //     [53.699813099076165, 9.932808452732685]
+  // ]).addTo(map);
+  //
+  //
+  //
+  //
+  //
+  // var polygon = L.polygon([
+  //     [53.599813099076165, 9.932808452732685],
+  //     [53.599813099076165, 9.942808452732685]
+  // ]).addTo(map);
+
+  async function get_coordinates(name)
+  {
+    response = await fetch("/com_offer_coordinates", {method: "POST"})
+    coordinates = await response.json()
+    for (i = 0; i < coordinates.length;  i++)
+    {
+    var marker = L.marker(coordinates[i][1]).addTo(map);
+    var marker = L.marker(coordinates[i][2]).addTo(map);
+  }
+  // return await coordinates;
+}
+
+// (async () => {
+	// console.log(await get_coordinates())
+  // coordinates = await get_coordinates()
+  // console.log(coordinates.length);
+  // for (i = 0; i < coordinates.length;  i++)
+  // {
+  //   // console.log(coordinates[i][1]);
+  //
+  //   // if (i == 1)
+  //   // {
+  //   //   var marker = L.circle(coordinates[i][1], 500, {fillColor:"green"}).addTo(map);
+  //   //
+  //   //   var marker =  L.circle(coordinates[i][2], 500, {fillColor:"green"}).addTo(map);
+  //   //
+  //   //
+  //   //
+  //   // }
+  //   // else
+  //   // {
+  //   //   var marker = L.marker(coordinates[i][1]).addTo(map);
+  //   //   var marker = L.marker(coordinates[i][2]).addTo(map);
+  //   // }
+  //
+  //
+  //
+  //     if ((i < coordinates.length - 1) && (i >= 0))
+  //     {
+  //       console.log("entered if case");
+  //       var polygon = L.polygon([
+  //           coordinates[i][1],
+  //           coordinates[i + 1][1]
+  //       ]).addTo(map);
+  //     }
+  // }
+
+
+//
+// })()
+
+
+
+
+
+})
 function openTab(name)
 {
   var i;
