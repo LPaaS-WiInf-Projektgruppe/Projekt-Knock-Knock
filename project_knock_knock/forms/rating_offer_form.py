80ea506ae2316206e80cef95b8cdaa5b3d586031
joe-hannes <johannes.idelberger@googlemail.com>
2022-01-16 19:45:23 +0100

    made the the rating look like a rating :D

--- project_knock_knock/forms/rating_offer_form.py
+++ project_knock_knock/forms/rating_offer_form.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import SubmitField
+from wtforms.fields import SubmitField, SelectField
 from wtforms.fields.html5 import IntegerRangeField
 from wtforms.validators import DataRequired, InputRequired, NumberRange, Regexp
 
-
+select_choices = {1:"1", 2:"2", 3:"3", 4:"4", 5:"5"}
 class RatingForm(FlaskForm):
-    rating = IntegerRangeField('Rating', validators=[NumberRange(min=0, max=5)])
+    rating = SelectField('Rating', validators=[NumberRange(min=0, max=5)], choices = select_choices, coerce=int)
     submit = SubmitField("Save Rating")
